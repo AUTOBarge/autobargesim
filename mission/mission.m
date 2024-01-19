71df5f28b92e41b1db2a3c8014e2d63b0b3c76ac
dhanikamahipala <dhanika1671@gmail.com>
2024-01-19 11:13:06 +0100

    initial commit

---
+++ mission/mission.m
@@ -0,0 +1,23 @@
+classdef mission
+    properties
+        pos_matrix
+        speed_vec
+    end
+    
+    methods
+        function M = mission(pos, speed)
+        % MISSION Creates a mission plan.
+        % M = MISSION(pos, speed) creates a mission plan matrix of shape:
+        %       
+        %       M = [x_1, y_1, U_1
+        %                 ...
+        %            x_n, y_n, U_n]
+        %            
+        %            where n is the number of waypoints
+        %
+        % For more information, see
+
+        end
+    end
+end
+
