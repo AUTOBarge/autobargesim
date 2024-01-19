a05bccfa08bf98eca486947e746a26742f3f0a7a
dhanikamahipala <dhanika1671@gmail.com>
2024-01-19 12:00:21 +0100

    class method completed

--- mission_planner/mission.m
+++ mission_planner/mission.m
@@ -1,22 +1,46 @@
 classdef mission
     properties
-        pos_matrix
+        pos_vec
         speed_vec
+        mission_plan
     end
     
     methods
         function M = mission(pos, speed)
         % MISSION Creates a mission plan.
-        % M = MISSION(pos, speed) creates a mission plan matrix of shape:
+        % M = MISSION(pos, speed) creates a mission plan matrix of shape (: x 3)
         %       
-        %       M = [x_1, y_1, U_1
-        %                 ...
-        %            x_n, y_n, U_n]
+        % OUTPUTS:
+        %       M -> [x_1, y_1, U_1
+        %                  ...
+        %             x_n, y_n, U_n]
         %            
-        %            where n is the number of waypoints
+        %             where n is the number of waypoints. x and y are in
+        %             'm' while U is in 'm/s' unit.
         %
-        % For more information, see
+        % INPUTS:
+        %       pos -> position coordinate array of the waypoints in 'm'.
+        %              size (: x 2) | matrix | double
+        %       speed -> speed vector containing the expected speed at each
+        %                waypoint in 'm/s'
+        %                size (: x 1) | vector | double
+        %
+        
+            arguments
+                pos (:,2) double
+                speed (:,1) double 
+            end
+            
+            % Validation
+            if ~isequal(length(pos(:, 1)), length(speed))
+                msg = 'Length of first input must equal length of second input.';
+                error(msg)
+            end
 
+            % Function code
+            M.pos_vec = pos;
+            M.speed_vec = speed;
+            M.mission_plan = [M.pos_vec, M.speed_vec];
         end
     end
 end
