e09931647cd0acd018d9a909b1a5f2915d3d40b2
dhanikamahipala <dhanika1671@gmail.com>
2024-02-14 17:56:02 +0100

    implemented collision avoidance related classes

--- colav/sbmpc.m
+++ colav/sbmpc.m
@@ -1,8 +1,8 @@
-classdef sbmpc
-    properties (Constant, Hidden, Access=private)
+classdef sbmpc < colav
+    properties (Constant, Hidden)
         default_D_CLOSE_ = 20 % distance for an nearby obstacle [m]
         default_D_SAFE_ = 10 % distance of safety zone [m]
-        default_D_INIT_ = SBMPC.default_D_CLOSE_ * 3
+        default_D_INIT_ = sbmpc.default_D_CLOSE_ * 3
 
         default_KAPPA_ = 7.0 % cost function parameter
         default_PHI_AH_ = deg2rad(68.5) % colregs angle - ahead [deg]
@@ -25,65 +25,153 @@ classdef sbmpc
         default_U_ca_ = [0, 0.5, 1.0]  % control behaviors - speed factor
     end
 
-    properties
+    properties 
         tuning_param % tuning parameters
-        T % prediction horizon
-        dt % prediction sampletime
     end
     
-    methods (Access=public)
+    methods
         function sbmpcObj = sbmpc(T, dt, varargin)
-            validateattributes(T, {'double'}, {'scalar', '>', 0});
-            validateattributes(dt, {'double'}, {'scalar', '>', 0, '<', T});
+            % Creates an instance of the sbmpc class.
+            % sbmpcObj = sbmpc(T, dt, varargin)
+            %
+            % INPUTS:
+            %       T -> Prediction time horizon of the algorithm in
+            %            seconds.
+            %            scalar | double
+            %       dt -> Prediction sample time of the algorithm in
+            %             seconds.
+            %             scalar | double
+            %       optional -> Name-value arguments for the tuning
+            %                   parameters of the SBMPC algorithm.
+            %
 
             p = inputParser;
             p.KeepUnmatched = true;
-            addParameter(p, 'D_CLOSE_', LOSguidance.default_K_p, @(x) validateattributes(x, {'double'}, {'scalar'}));
-            addParameter(p, 'D_SAFE_', LOSguidance.default_K_p, @(x) validateattributes(x, {'double'}, {'scalar'}));
-            addParameter(p, 'D_INIT_', LOSguidance.default_K_p, @(x) validateattributes(x, {'double'}, {'scalar'}));
-            addParameter(p, 'KAPPA_', LOSguidance.default_K_p, @(x) validateattributes(x, {'double'}, {'scalar'}));
-            addParameter(p, 'PHI_AH_', LOSguidance.default_K_p, @(x) validateattributes(x, {'double'}, {'scalar'}));
-            addParameter(p, 'PHI_OT_', LOSguidance.default_K_p, @(x) validateattributes(x, {'double'}, {'scalar'}));
-            addParameter(p, 'PHI_HO_', LOSguidance.default_K_p, @(x) validateattributes(x, {'double'}, {'scalar'}));
-            addParameter(p, 'PHI_CR_', LOSguidance.default_K_p, @(x) validateattributes(x, {'double'}, {'scalar'}));
-            addParameter(p, 'K_COLL_', LOSguidance.default_K_p, @(x) validateattributes(x, {'double'}, {'scalar'}));
-            addParameter(p, 'P_', LOSguidance.default_K_p, @(x) validateattributes(x, {'double'}, {'scalar'}));
-            addParameter(p, 'Q_', LOSguidance.default_K_p, @(x) validateattributes(x, {'double'}, {'scalar'}));
-            addParameter(p, 'K_P_', LOSguidance.default_K_p, @(x) validateattributes(x, {'double'}, {'scalar'}));
-            addParameter(p, 'K_CHI_SB_', LOSguidance.default_K_p, @(x) validateattributes(x, {'double'}, {'scalar'}));
-            addParameter(p, 'K_CHI_P_', LOSguidance.default_K_p, @(x) validateattributes(x, {'double'}, {'scalar'}));
-            addParameter(p, 'K_DP_', LOSguidance.default_K_p, @(x) validateattributes(x, {'double'}, {'scalar'}));
-            addParameter(p, 'K_DCHI_SB_', LOSguidance.default_K_p, @(x) validateattributes(x, {'double'}, {'scalar'}));
-            addParameter(p, 'K_DCHI_P_', LOSguidance.default_K_p, @(x) validateattributes(x, {'double'}, {'scalar'}));
-            addParameter(p, 'Chi_ca_', LOSguidance.default_K_p, @(x) validateattributes(x, {'double'}, {'scalar'}));
-            addParameter(p, 'U_ca_', LOSguidance.default_K_p, @(x) validateattributes(x, {'double'}, {'scalar'}));
+            % tuning parameters
+            addParameter(p, 'D_CLOSE_', sbmpc.default_D_CLOSE_, @(x) validateattributes(x, {'double'}, {'scalar'}));
+            addParameter(p, 'D_SAFE_', sbmpc.default_D_SAFE_, @(x) validateattributes(x, {'double'}, {'scalar'}));
+            addParameter(p, 'D_INIT_', sbmpc.default_D_INIT_, @(x) validateattributes(x, {'double'}, {'scalar'}));
+            addParameter(p, 'KAPPA_', sbmpc.default_KAPPA_, @(x) validateattributes(x, {'double'}, {'scalar'}));
+            addParameter(p, 'PHI_AH_', sbmpc.default_PHI_AH_, @(x) validateattributes(x, {'double'}, {'scalar'}));
+            addParameter(p, 'PHI_OT_', sbmpc.default_PHI_OT_, @(x) validateattributes(x, {'double'}, {'scalar'}));
+            addParameter(p, 'PHI_HO_', sbmpc.default_PHI_HO_, @(x) validateattributes(x, {'double'}, {'scalar'}));
+            addParameter(p, 'PHI_CR_', sbmpc.default_PHI_CR_, @(x) validateattributes(x, {'double'}, {'scalar'}));
+            addParameter(p, 'K_COLL_', sbmpc.default_K_COLL_, @(x) validateattributes(x, {'double'}, {'scalar'}));
+            addParameter(p, 'P_', sbmpc.default_P_, @(x) validateattributes(x, {'double'}, {'scalar'}));
+            addParameter(p, 'Q_', sbmpc.default_Q_, @(x) validateattributes(x, {'double'}, {'scalar'}));
+            addParameter(p, 'K_P_', sbmpc.default_K_P_, @(x) validateattributes(x, {'double'}, {'scalar'}));
+            addParameter(p, 'K_CHI_SB_', sbmpc.default_K_CHI_SB_, @(x) validateattributes(x, {'double'}, {'scalar'}));
+            addParameter(p, 'K_CHI_P_', sbmpc.default_K_CHI_P_, @(x) validateattributes(x, {'double'}, {'scalar'}));
+            addParameter(p, 'K_DP_', sbmpc.default_K_DP_, @(x) validateattributes(x, {'double'}, {'scalar'}));
+            addParameter(p, 'K_DCHI_SB_', sbmpc.default_K_DCHI_SB_, @(x) validateattributes(x, {'double'}, {'scalar'}));
+            addParameter(p, 'K_DCHI_P_', sbmpc.default_K_DCHI_P_, @(x) validateattributes(x, {'double'}, {'scalar'}));
+            addParameter(p, 'Chi_ca_', sbmpc.default_Chi_ca_, @(x) validateattributes(x, {'double'}, {'vector'}));
+            addParameter(p, 'U_ca_', sbmpc.default_U_ca_, @(x) validateattributes(x, {'double'}, {'vector'}));
+            
+            % internal vessel model parameters
+            addParameter(p, 'T_chi', sbmpc.default_U_ca_);
+            addParameter(p, 'T_U', sbmpc.default_U_ca_);
             parse(p, varargin{:});
 
-            sbmpcObj.T = T;
-            sbmpcObj.dt = dt;
+            sbmpcObj = sbmpcObj@colav(T, dt);
             sbmpcObj.tuning_param = struct(...
-                'D_CLOSE_', SBMPC.default_D_CLOSE_,...
-                'D_SAFE_', SBMPC.default_D_SAFE_,...
-                'D_INIT_', SBMPC.default_D_INIT_,...
-                'KAPPA_', SBMPC.default_KAPPA_,...
-                'PHI_AH_', SBMPC.default_PHI_AH_,...
-                'PHI_OT_', SBMPC.default_PHI_OT_,...
-                'PHI_HO_', SBMPC.default_PHI_HO_,...
-                'PHI_CR_', SBMPC.default_PHI_CR_,...
-                'K_COLL_', SBMPC.default_K_COLL_,...
-                'P_', SBMPC.default_P_,...
-                'Q_', SBMPC.default_Q_,...
-                'K_P_', SBMPC.default_K_P_,...
-                'K_CHI_SB_', SBMPC.default_K_CHI_SB_,...
-                'K_CHI_P_', SBMPC.default_K_CHI_P_,...
-                'K_DP_', SBMPC.default_K_DP_,...
-                'K_DCHI_SB_', SBMPC.default_K_DCHI_SB_,...
-                'K_DCHI_P_', SBMPC.default_K_DCHI_P_,...
-                'Chi_ca_', SBMPC.default_Chi_ca_,...
-                'U_ca_', SBMPC.default_U_ca_);
+                'D_CLOSE_', p.Results.D_CLOSE_,...
+                'D_SAFE_', p.Results.D_SAFE_,...
+                'D_INIT_', p.Results.D_INIT_,...
+                'KAPPA_', p.Results.KAPPA_,...
+                'PHI_AH_', p.Results.PHI_AH_,...
+                'PHI_OT_', p.Results.PHI_OT_,...
+                'PHI_HO_', p.Results.PHI_HO_,...
+                'PHI_CR_', p.Results.PHI_CR_,...
+                'K_COLL_', p.Results.K_COLL_,...
+                'P_', p.Results.P_,...
+                'Q_', p.Results.Q_,...
+                'K_P_', p.Results.K_P_,...
+                'K_CHI_SB_', p.Results.K_CHI_SB_,...
+                'K_CHI_P_', p.Results.K_CHI_P_,...
+                'K_DP_', p.Results.K_DP_,...
+                'K_DCHI_SB_', p.Results.K_DCHI_SB_,...
+                'K_DCHI_P_', p.Results.K_DCHI_P_,...
+                'Chi_ca_', p.Results.Chi_ca_,...
+                'U_ca_', p.Results.U_ca_);
+        end
+        function [chi_c, U_c, chi_m, U_m] = run_sbmpc(self, x, chi_d, U_d, chi_m_last, U_m_last, x_ts)
+            % Calculate the modifications for the course angle and speed
+            % commands from the guidance controller.
+            %
+            % [chi_c, U_c, chi_m, U_m] = run_sbmpc(x, chi_d, U_d, chi_m_last, U_m_last, x_ts)
+            %
+            % INPUTS:
+            %       x -> own-ship state [x, y, chi, U]
+            %            size (1 x 4) | vector | double
+            %       chi_d -> course angle command from the guidance
+            %                controller
+            %                scalar | double
+            %       U_d -> speed command from the guidance controller
+            %              scalar | double
+            %       chi_m_last -> course modification from the SBMPC at the
+            %                     previous time step.
+            %                     scalar | double
+            %       U_m_last -> speed modification from the SBMPC at the
+            %                   previous time step
+            %                   scalar | double
+            %       x_ts -> target-ship states:
+            %               [x_n, y_n, chi_n, U_n] where n is the vessel ID
+            %               size (: x 4) | matrix | double
+            %
+
+            validateattributes(x, {'double'}, {'vector', 'ncols', 4})
+            validateattributes(chi_d, {'double'}, {'scalar'})
+            validateattributes(U_d, {'double'}, {'scalar'})
+            validateattributes(chi_m_last, {'double'}, {'scalar'})
+            validateattributes(U_m_last, {'double'}, {'scalar'})
+            validateattributes(x, {'double'}, {'2d', 'ncols', 4})
+
+            n_samples = utils.getNumSamples(self.dt, self.T);
+            Chi_ca_ = self.tuning_param.Chi_ca_;
+            U_ca_ = self.tuning_param.U_ca_;
+
+            cost_k_min = inf;
+            for i = 1:length(Chi_ca_)
+                for j = 1:length(U_ca_)
+                    cost_i_max = -1;
+                    
+                    ownship_traj = self.calcVesselTraj(x, [chi_d + Chi_ca_(i), U_d * U_ca_(j)], self.dt, n_samples);
+                    
+                    num_ts = numel(x_ts(:, 1));
+                    for idx = 1:num_ts
+                        targetship_traj = self.calcVesselTraj(x_ts(idx, :), x_ts(idx, 3:4), self.dt, n_samples);
+                        
+                        [~, ~, ~, ~, ~, ~, colregs_cost] = self.calc_cost_COLREGS(ownship_traj, targetship_traj, n_samples);
+                        [~, ~, collision_cost] = self.calc_cost_collision(ownship_traj, targetship_traj, self.dt, n_samples);
+        
+                        cost_i = max(colregs_cost + collision_cost);
+            
+                        if cost_i > cost_i_max
+                            cost_i_max = cost_i;
+                        end
+                    end
+        
+                    if cost_i_max == -1 % when there are no other obstacles, turns to both sides must be equally penalized
+                        F = K_P_ * (1 - U_ca_(j))^2 + K_CHI_P_ * Chi_ca_(i)^2;
+                    else
+                        F = self.calc_cost_maneuvering(chi_d, U_d, Chi_ca_(i), U_ca_(j), chi_m_last, U_m_last);
+                    end
+        
+                    tot_cost = cost_i_max + F;
+        
+                    if tot_cost < cost_k_min
+                        cost_k_min = tot_cost;
+                        chi_m = Chi_ca_(i); 
+                        U_m = U_ca_(j);
+                    end
+                end
+            end
+            chi_c = utils.wrap_angle_to_pmpi(chi_d + chi_m);
+            U_c = U_d * U_m;
         end
     end
-    methods (Access=private)
+    methods (Hidden)
         function F = calc_cost_maneuvering(self, chi_d, U_d, chi_m, U_m, chi_m_last, U_m_last)
             % INPUTS:
             %        chi_d -> desired course angle in radians
@@ -166,7 +254,7 @@ classdef sbmpc
             cost_N = zeros(1, N);
 
             for i = 1:N
-                [d, v_o, v_s] = SBMPC.extractInfoFromTrajsAtSampleN(os_traj, ts_traj, i);
+                [d, v_o, v_s] = self.extractInfoFromTrajsAtSampleN(os_traj, ts_traj, i);
                 
                 % Check whether it is CLOSE
                 CL_N(i) = norm(d) <= D_CLOSE_;
@@ -176,7 +264,7 @@ classdef sbmpc
     
                 % Obstacle on starboard side
                 os_psi_ = os_traj(i, 3);
-                phi = MISC.wrap_angle_diff_to_pmpi(atan2(d(2), d(1)), os_psi_);
+                phi = utils.wrap_angle_diff_to_pmpi(atan2(d(2), d(1)), os_psi_);
                 SB_N(i) = phi <= 0;
     
                 % Obstacle Head-on
