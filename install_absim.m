6edb3cb348e078be50d73954899ca82fbca081a5
Abhishek Dhyani <dhyaniabhishek13@gmail.com>
2024-06-26 09:09:22 +0200

    Added a script to install the simulator.

---
+++ install_absim.m
@@ -0,0 +1,49 @@
+function install_absim()
+%To install the toolbox, simply run this matlab file or type 'install_absim' in the MATLAB command window.
+    fprintf('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n');
+	fprintf('AUTOBargeSim: MATLAB toolbox for the design and analysis of the Guidance, Navigation and Control System for autonomous inland vessels.\n');
+	fprintf('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n');
+	
+    folders = {'control', 'guidance', 'maps', 'mission_planner', 'model&actuatorClass'};
+    
+    currentFolder = pwd;
+    
+    % Adding the folders and subfolders to the MATLAB path
+    try
+        for i = 1:length(folders)
+            folderPath = fullfile(currentFolder, folders{i});
+            addpath(genpath(folderPath));
+        end
+        fprintf('Folders and subfolders have been added to the MATLAB path.\n');
+        
+        % Adding third-party package (Casadi)
+         casadiInstalled = input('Would you like to install the Casadi package? (Casadi is required for using some features of the AUTOBargeSim toolbox) (y/n): ', 's');
+         if strcmpi(casadiInstalled, 'y')
+			casadiPath = fullfile(currentFolder,'prerequisites','casadi');
+			addpath(casadiPath);
+			fprintf('Casadi has been added to the MATLAB path.\n');
+         else
+            fprintf('Proceeding without installing the Casadi package... .\n');
+         end
+        
+		% Checking for the installation of MATLAB Control System Toolbox
+        if license('test', 'Control_Toolbox')
+            fprintf('Control System Toolbox is available.\n');
+        else
+            error('Control System Toolbox is not available. Please install it first.');
+        end
+        
+        % Saving the path changes
+        status = savepath;
+        if status == 0
+            fprintf('Path changes have been saved successfully.\n');
+        else
+            error('Failed to save the path changes. You may need administrator rights.');
+        end
+        
+    catch ME
+        % Error handling
+        fprintf('An error occurred: %s\n', ME.message);
+        fprintf('Ensure you have the necessary permissions and that the folders exist.\n');
+    end
+end
